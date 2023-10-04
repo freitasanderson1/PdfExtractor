15f353a9ab9244eb9f1bbd1a5f7b5fe489f28322
Anderson Freitas <freitas.dev@proton.me>
2023-10-04 20:41:03 -0300

    :art: Código refatorado e salvando informações necessárias
    Código refatorado e salvando informações necessárias

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ reader = PdfReader('EM BEATRIZ RODRIGUES.pdf')
 totalPaginas = len(reader.pages)
 listaCabecalho = ['ESTADO DO TOCANTINS', 'PREFEITURA MUNICIPAL DE PALMAS', 'RELAÇÃO DE ALUNOS POR TURMA']
 
-df = pd.DataFrame(columns=['Turma', 'Ord.', 'Idmatricula', 'Nome', 'Data de Nascimento', 'Idade', 'Situação Matrícula'])
+df = pd.DataFrame(columns=['Turma', 'Idmatricula', 'Nome'])
 
 listPaginas = list()
 turma = None
-
+listaAlunos = list()
 for index in range(totalPaginas):
     page = reader.pages[index]
 
@@ -19,44 +19,38 @@ for index in range(totalPaginas):
 
     listaTexto = text.split('\n')
 
-    del listaTexto[0:3]
+    del listaTexto[0:4]
 
     for linha in listaTexto:
         if "Turma:" in linha:
-            turma = linha.split(":")[1].strip()
+            turma = linha.split(":")[1].replace('Período letivo','')
+
+        elif "Idmatricula" in linha:
+            pass
+
         elif linha.strip():
             partes = linha.split()
-            ordem = partes[0]
-            matricula = partes[1]
-            situacao = partes[-1] 
-            partes.pop()
+            partes = linha.split('/')
             
-            if situacao == "REPROVADO" or situacao == "CURSANDO":
-                nome_data_idade = ' '.join(partes[2:-1])
-            else:
-                nome_data_idade = ' '.join(partes[2:])
+            partes2 = partes[0].split(' ')
+            
+            partes2.pop(0)
+            partes2.pop(-1)
+
             
             # regex totalmente insalubre, nao confie
-            match = re.search(r'(\d{2}/\d{2}/\d{4}) (\d+ Anos, \d+ Meses e \d+ Dias)', nome_data_idade)
-            if match:
-                dataNascimento = match.group(1)
-                idade = match.group(2)
-                nome = nome_data_idade.replace(match.group(0), '').strip()
-            else:
-                dataNascimento = ''
-                idade = ''
-                nome = nome_data_idade
-            
+            matricula = partes2[0]
+            partes2.pop(0)
+            nome = ' '.join(map(str,partes2))
             aluno_dict = {
                 'Turma': turma,
-                'Ord.': ordem,
                 'Idmatricula': matricula,
                 'Nome': nome,
-                'Data de Nascimento': dataNascimento,
-                'Idade': idade,
-                'Situação Matrícula': situacao
             }
             
-            df = pd.concat([df, pd.DataFrame([aluno_dict])], ignore_index=True)
+            # print(aluno_dict)
+            
+            listaAlunos.append(aluno_dict)
+#             df = pd.concat([df, pd.DataFrame([aluno_dict])], ignore_index=True)
 
-df.to_csv('alunos.csv', index=False)
+# df.to_csv('alunos.csv', index=False)
